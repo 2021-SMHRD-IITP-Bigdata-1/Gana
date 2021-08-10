fec55fb98980cd84aa45c4f21334302d90d6e59e
smhrd <smhrd@DESKTOP-5PT8J15>
2021-08-10 16:46:31 +0900

    김창주 / 성훈이 파일 커밋

---
+++ Gana Real Project/src/main/java/com/controller/lectureDAO.java
@@ -0,0 +1,64 @@
+package com.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class lectureDAO {
+
+	public ArrayList<lectureVO> selectAll() {
+		// 모든 회원들의 정보를 돌려주는 selectAll
+		ArrayList<lectureVO> arr = new ArrayList<lectureVO>();
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		String CERT_NAME = "전기기사";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String dbid = "cgi_4_2";
+			String dbpw = "smhrd2";
+			conn = DriverManager.getConnection(url, dbid, dbpw);
+
+			String sql = "SELECT LEC_INFO, LEC_TEACH, LEC_FREE, LEC_LINK, LEC_TEST, LEC_COST, LEC_NUM, LEC_BF, LEC_DATE, LEC_SUB, LEC_NAME, LEC_SITE FROM LECTURE WHERE CERT_ID = ( SELECT CERT_ID FROM CERTIFICATION WHERE CERT_NAME =?)";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, "정보처리기사");
+
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				String info = rs.getString(1);
+				String teach = rs.getString(2);
+				int free = rs.getInt(3);
+				String link = rs.getString(4);
+				int test = rs.getInt(5);
+				int cost = rs.getInt(6);
+				String num = rs.getString(7);
+				int bf = rs.getInt(8);
+				String date = rs.getString(9);
+				int sub = rs.getInt(10);
+				String name = rs.getString(11);
+				String site = rs.getString(12);
+
+				arr.add(new lectureVO(info, teach, free, link, test, cost, num, bf, date, sub, name, site));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				psmt.close();
+				conn.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return arr;
+	}
+}
---
+++ Gana Real Project/src/main/java/com/controller/lectureVO.java
@@ -0,0 +1,106 @@
+package com.controller;
+
+public class lectureVO {
+	private String info;
+	private String teach;
+	private int free;
+	private String link;
+	private int test;
+	private int cost;
+	private String num;
+	private int bf;
+	private String date;
+	private int sub;
+	private String name;
+	private String site;
+	
+	public lectureVO(String info, String teach, int free, String link, int test, int cost, String num, int bf,
+			String date, int sub, String name, String site) {
+		super();
+		this.info = info;
+		this.teach = teach;
+		this.free = free;
+		this.link = link;
+		this.test = test;
+		this.cost = cost;
+		this.num = num;
+		this.bf = bf;
+		this.date = date;
+		this.sub = sub;
+		this.name = name;
+		this.site = site;
+	}
+	
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getTeach() {
+		return teach;
+	}
+	public void setTeach(String teach) {
+		this.teach = teach;
+	}
+	public int getFree() {
+		return free;
+	}
+	public void setFree(int free) {
+		this.free = free;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public int getTest() {
+		return test;
+	}
+	public void setTest(int test) {
+		this.test = test;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public int getBf() {
+		return bf;
+	}
+	public void setBf(int bf) {
+		this.bf = bf;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getSub() {
+		return sub;
+	}
+	public void setSub(int sub) {
+		this.sub = sub;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+}
